7a8de4f4f7701d00ea18aa5d5b67e3726e222c2a
Ekamjot Singh <160436305+ekam-brar-1@users.noreply.github.com>
2025-03-19 12:22:58 -0600

    added getitem and additem

--- app/page.js
+++ app/page.js
@@ -29,6 +29,9 @@ export default function Page() {
       <button className="bg-transparent hover:bg-blue-500 m-2 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
         <Link href="./week-9"> Week-9</Link>
       </button>
+      <button className="bg-transparent hover:bg-blue-500 m-2 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+        <Link href="./week-10"> Week-10</Link>
+      </button>
     </main>
   );
 }
--- app/week-10/shopping-list/page.js
+++ app/week-10/shopping-list/page.js
@@ -2,11 +2,17 @@
 import { useState } from "react";
 import MealIdeas from "./meal-ideas";
 import ItemList from "./item-list"; // Make sure to import ItemList
-import itemData from "./item.json";
+import { getItems } from "../_services/shopping-list-service";
+import { addItem } from "../_services/shopping-list-service";
+import { useEffect } from "react";
 import { useUserAuth } from "../_utils/auth-context";
 export default function App() {
   const [ingredient, setIngredient] = useState("");
-  const [items, setItems] = useState(itemData);
+  const [items, setItems] = useState([]);
+  const loadItems = async () => {
+    const items = await getItems();
+    setItems(items);
+  };
   // Define onselectItem function
   const { user } = useUserAuth();
   const onselectItem = (item) => {
